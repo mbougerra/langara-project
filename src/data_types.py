016d314f5869d06923f74e907a381f97647e0625
Mourad Bouguerra <mbouguerra@langara.ca>
2022-05-19 17:16:17 -0700

    Adding more code

--- src/data_types.py
+++ src/data_types.py
@@ -5,3 +5,7 @@ print(f'The size of the list x is {sys.getsizeof(x)} byte(s)')
 y=1e6
 print(f'y={y}')
 print(f'The data type of y is {type(y)}')
+z=('a','b','c')
+w=['a','b','c']
+print(f'The size of the tuple {z} is {sys.getsizeof(z)} bytes')
+print(f'The size of the list {w} is {sys.getsizeof(w)} bytes')
